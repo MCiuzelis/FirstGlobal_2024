41f6364fad4c78c6efd8dbf2d4efccfa06f6dbb9
matasciuzelis <matas.ciuzelis@gmail.com>
2024-08-15 21:37:37 +0300

    Added new commands, ready for first real testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -13,19 +13,18 @@ public class IntakeSubsystem extends SubsystemBase {
     public static double angle_i = 0;
     public static double angle_d = 0;
 
-    public static double velocity_p = 0;
-    public static double velocity_i = 0;
-    public static double velocity_d = 0;
+    public static double angle_DOWN = 0;
+    public static double angle_UP = 0;
 
-    public static double startingPosition = 0;
-    public static double preparedPosition = 0;
-    public static double pickupPosition = 0;
+    public static double nominalSpeed = 0.75;
+
+    public static double angleOffsetMultiplier = 10;
 
     private double targetAngle = 0;
-    private double targetVelocity = 0;
+    private double targetSpeed = 0;
+    private double angleOffset = 0;
 
     private PIDController anglePID = new PIDController(angle_p, angle_i, angle_d);
-    private PIDController velocityPID = new PIDController(velocity_p, velocity_i, velocity_d);
 
     RobotHardware robot;
     Telemetry telemetry;
@@ -36,45 +35,41 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
 
-    public void setIntakeAngle(INTAKE_ANGLE angle){
+    public void setAngle(INTAKE_ANGLE angle){
         switch (angle){
-            case STARTING_POSITION:
-                targetAngle = startingPosition;
+            case DOWN:
+                targetAngle = angle_DOWN;
                 break;
-            case PREPARED_POSITION:
-                targetAngle = preparedPosition;
-                break;
-            case PICKUP_POSITION:
-                targetAngle = pickupPosition;
+            case UP:
+                targetAngle = angle_UP;
                 break;
         }
     }
 
-    public void setIntakeTargetVelocity(double velocity){
-        targetVelocity = velocity;
+    public void setAngleOffset(double offset){
+        if (targetAngle == angle_UP) angleOffset = offset * angleOffsetMultiplier;
+        else angleOffset = 0;
+    }
+
+    public void setSpeed(double speed){
+        targetSpeed = speed;
     }
 
     public void periodic(){
         anglePID.setPID(angle_p, angle_i, angle_d);
-        velocityPID.setPID(velocity_p, velocity_i, velocity_d);
 
-        double currentAngle = robot.intake_Angle.getRotation();
-        double currentVelocity = robot.intakeSpeed.getVelocity();
-
-        double anglePower = anglePID.calculate(currentAngle, targetAngle);
-        double velocityPower = velocityPID.calculate(currentVelocity, targetVelocity);
+        double currentAngle = Math.toDegrees(robot.encoder_intake_Angle.getRotation());
+        double anglePower = anglePID.calculate(currentAngle, targetAngle - angleOffset);
 
         robot.intake_AngleMotor.setPower(anglePower);
-        robot.intake_spinyMotor.setPower(velocityPower);
+        robot.intake_spinyMotor.setPower(targetSpeed);
 
         telemetry.addData("currentIntakeAngle: ", currentAngle);
-        telemetry.addData("currentIntakeVelocity: ", currentVelocity);
     }
 
 
     public enum INTAKE_ANGLE{
-        STARTING_POSITION,
-        PREPARED_POSITION,
-        PICKUP_POSITION
+        DOWN,
+        UP
     }
 }
