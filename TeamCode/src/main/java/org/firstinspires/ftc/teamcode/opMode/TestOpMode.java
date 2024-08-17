5861f5d94431b8869ded2edb827ffefd3e5a75f8
matasciuzelis <matas.ciuzelis@gmail.com>
2024-08-17 19:14:29 +0300

    Sorta working intake command and drivebase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/TestOpMode.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opMode;
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.outoftheboxrobotics.photoncore.Photon;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -21,14 +22,16 @@ public class TestOpMode extends TeleOpBase {
                 .whenPressed(()-> schedule(new setIntakeState(intake, setIntakeState.IntakeState.UP)));
         driver.getGamepadButton(driver.dpadDown)
                 .whenPressed(()-> schedule(new setIntakeState(intake, setIntakeState.IntakeState.DOWN)));
-        driver.getGamepadButton(driver.triangle)
-                .whenPressed(()-> schedule(new setLiftHeightCommand(lift, LiftSubsystem.TARGET_POSITION.UP)));
-        driver.getGamepadButton(driver.cross)
-                .whenPressed(()-> schedule(new setLiftHeightCommand(lift, LiftSubsystem.TARGET_POSITION.DOWN)));
-        driver.getGamepadButton(driver.square)
-                .whenPressed(()-> schedule(new InstantCommand(()-> lift.offsetTarget(10))));
-        driver.getGamepadButton(driver.circle)
-                .whenPressed(()-> schedule(new InstantCommand(()-> lift.offsetTarget(-10))));
+
+//----------UNCOMMENT WHEN LIFT PID IS FIXED----------------------------------
+//        driver.getGamepadButton(driver.triangle)
+//                .whenPressed(()-> schedule(new setLiftHeightCommand(lift, LiftSubsystem.TARGET_POSITION.UP)));
+//        driver.getGamepadButton(driver.cross)
+//                .whenPressed(()-> schedule(new setLiftHeightCommand(lift, LiftSubsystem.TARGET_POSITION.DOWN)));
+//        driver.getGamepadButton(driver.square)
+//                .whenPressed(()-> schedule(new InstantCommand(()-> lift.offsetTarget(10))));
+//        driver.getGamepadButton(driver.circle)
+//                .whenPressed(()-> schedule(new InstantCommand(()-> lift.offsetTarget(-10))));
     }
 
     @Override
@@ -38,8 +41,16 @@ public class TestOpMode extends TeleOpBase {
     @Override
     public void Loop() {
         robot.controlHub.pullBulkData();
-        tank.loop(driver.getGamepadInput());
         intake.setAngleOffset(gamepad1.left_trigger);
+
+        CommandScheduler.getInstance().run();
+        tank.loop(driver.getGamepadInput());
+
+        telemetry.addData("driveLeft", robot.encoder_driveBaseLeft.getRotation());
+        telemetry.addData("driveRight", robot.encoder_driveBaseRight.getRotation());
+        telemetry.addData("intake angle", robot.encoder_intake_Angle.getRotation());
+        telemetry.addData("lift", robot.encoder_liftPosition.getRotation());
+
 
         if (gamepad1.left_bumper){
             robot.liftMotor_Left.setPower(power);
@@ -53,6 +64,5 @@ public class TestOpMode extends TeleOpBase {
             robot.liftMotor_Left.setPower(0);
             robot.liftMotor_Right.setPower(0);
         }
-        //test
     }
 }
