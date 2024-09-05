c003649dc1eb01599afb28423407c6b1ae05f31c
matasciuzelis <matas.ciuzelis@gmail.com>
2024-09-05 17:45:04 +0300

    transfer working. Ready for field testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/kickBall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/kickBall.java
@@ -4,22 +4,39 @@ import com.arcrobotics.ftclib.command.ConditionalCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
 
+import org.firstinspires.ftc.teamcode.commands.wrappers.setFrontServoState;
 import org.firstinspires.ftc.teamcode.commands.wrappers.setIntakeAngleCommand;
 import org.firstinspires.ftc.teamcode.commands.wrappers.setIntakeSpeedCommand;
 import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.LiftSubsystem;
 
 public class kickBall extends SequentialCommandGroup {
-    public kickBall(IntakeSubsystem intake){
+    public kickBall(IntakeSubsystem intake, LiftSubsystem lift){
         addCommands(
                 new ConditionalCommand(
                         new SequentialCommandGroup(
                                 //IF HOLDING BALL
-                                new setIntakeSpeedCommand(intake, -1),
-                                new WaitCommand(300),
-                                new setIntakeAngleCommand(intake, IntakeSubsystem.INTAKE_ANGLE.SPIN_OUT),
-                                new WaitCommand(800),
-                                new setIntakeSpeedCommand(intake, 0),
-                                new setIntakeAngleCommand(intake, IntakeSubsystem.INTAKE_ANGLE.DOWN)
+                                new ConditionalCommand(
+                                        new SequentialCommandGroup(
+                                                //IF BALL INSIDE
+                                                new setIntakeSpeedCommand(intake, -1),
+                                                new WaitCommand(300),
+                                                new setIntakeAngleCommand(intake, IntakeSubsystem.INTAKE_ANGLE.SPIN_OUT),
+                                                new WaitCommand(800),
+                                                new setIntakeSpeedCommand(intake, 0),
+                                                new setIntakeAngleCommand(intake, IntakeSubsystem.INTAKE_ANGLE.DOWN),
+                                                new setFrontServoState(lift, LiftSubsystem.BUCKET_SERVO_POSITION.HOLD)
+                                        ),
+                                        new SequentialCommandGroup(
+                                                //IF BALL OUTSIDE
+                                                new setIntakeSpeedCommand(intake, 1),
+                                                new WaitCommand(600),
+                                                new setIntakeSpeedCommand(intake, 0)
+                                        ),
+                                        ()-> intake.getCurrentState().equals(IntakeSubsystem.INTAKE_ANGLE.HOLDING_BALL_INSIDE)
+                                )
+
+
                         ),
                         new SequentialCommandGroup(
                                 new setIntakeAngleCommand(intake, IntakeSubsystem.INTAKE_ANGLE.UP),
@@ -28,11 +45,13 @@ public class kickBall extends SequentialCommandGroup {
                                 new WaitCommand(300),
 
                                 new setIntakeAngleCommand(intake, IntakeSubsystem.INTAKE_ANGLE.DOWN),
-                                new setIntakeSpeedCommand(intake, 0)
+                                new setIntakeSpeedCommand(intake, 0),
+                                new setFrontServoState(lift, LiftSubsystem.BUCKET_SERVO_POSITION.HOLD)
                         ),
-                        ()-> intake.getCurrentState().equals(IntakeSubsystem.INTAKE_ANGLE.HOLDING_BALL)
+                        ()-> intake.getCurrentState().equals(IntakeSubsystem.INTAKE_ANGLE.HOLDING_BALL_INSIDE) ||
+                             intake.getCurrentState().equals(IntakeSubsystem.INTAKE_ANGLE.HOLDING_BALL_OUTSIDE)
                 )
         );
-        addRequirements(intake);
+        addRequirements(intake, lift);
     }
 }
